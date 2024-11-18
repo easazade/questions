176401cffa50125467fda81d4dcb3db8eba2eba7
Ali <easazade@gmail.com>
2024-11-18 14:29:09 +0100

    Init project

---
+++ example/questions_example.dart
@@ -0,0 +1,6 @@
+import 'package:questions/questions.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/questions_test.dart
@@ -0,0 +1,16 @@
+import 'package:questions/questions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
